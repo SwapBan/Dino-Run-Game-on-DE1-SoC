d95f059bf9321344041f452857f9cf8b4e407210
roshan2177 <rp3187@columbia.edu>
2025-04-23 16:32:35 -0500

    Create vga_ball4.sv

---
+++ vga_ball4.sv
@@ -0,0 +1,126 @@
+module vga_ball(
+    input logic         clk,
+    input logic         reset,
+    input logic [31:0]  writedata,
+    input logic         write,
+    input               chipselect,
+    input logic [8:0]   address,
+
+    output logic [7:0]  VGA_R, VGA_G, VGA_B,
+    output logic        VGA_CLK, VGA_HS, VGA_VS,
+                        VGA_BLANK_n,
+    output logic        VGA_SYNC_n
+);
+
+    logic [10:0] hcount;
+    logic [9:0]  vcount;
+
+    logic [15:0] dino_sprite_output, jump_sprite_output, duck_sprite_output;
+    logic [15:0] godzilla_sprite_output, scac_sprite_output;
+
+    logic [9:0] dino_sprite_addr, jump_sprite_addr, duck_sprite_addr;
+    logic [9:0] godzilla_sprite_addr, scac_sprite_addr;
+
+    logic [7:0] dino_x, dino_y;
+    logic [7:0] jump_x, jump_y;
+    logic [7:0] duck_x, duck_y;
+    logic [7:0] s_cac_x, s_cac_y;
+    logic [7:0] godzilla_x, godzilla_y;
+
+    logic [7:0] a, b, c;
+
+    vga_counters counters(
+        .clk50(clk),
+        .reset(reset),
+        .hcount(hcount),
+        .vcount(vcount),
+        .VGA_CLK(VGA_CLK),
+        .VGA_HS(VGA_HS),
+        .VGA_VS(VGA_VS),
+        .VGA_BLANK_n(VGA_BLANK_n),
+        .VGA_SYNC_n(VGA_SYNC_n)
+    );
+
+    dino_sprite_rom dino_rom(.clk(clk), .address(dino_sprite_addr), .data(dino_sprite_output));
+    dino_jump_rom jump_rom(.clk(clk), .address(jump_sprite_addr), .data(jump_sprite_output));
+    dino_duck_rom duck_rom(.clk(clk), .address(duck_sprite_addr), .data(duck_sprite_output));
+    dino_godzilla_rom godzilla_rom(.clk(clk), .address(godzilla_sprite_addr), .data(godzilla_sprite_output));
+    dino_s_cac_rom s_cac_rom(.clk(clk), .address(scac_sprite_addr), .data(scac_sprite_output));
+
+    always_ff @(posedge clk) begin
+        if (reset) begin
+            dino_x <= 8'd100;   dino_y <= 8'd100;
+            jump_x <= 8'd200;   jump_y <= 8'd150;
+            duck_x <= 8'd300;   duck_y <= 8'd200;
+            s_cac_x <= 8'd500;  s_cac_y <= 8'd100;
+            godzilla_x <= 8'd100; godzilla_y <= 8'd260;
+            a <= 8'hFF; b <= 8'hFF; c <= 8'hFF;
+        end else if (chipselect && write) begin
+            case (address)
+                9'd0: dino_x <= writedata[7:0];
+                9'd1: dino_y <= writedata[7:0];
+                9'd2: jump_x <= writedata[7:0];
+                9'd3: jump_y <= writedata[7:0];
+                9'd4: duck_x <= writedata[7:0];
+                9'd5: duck_y <= writedata[7:0];
+                9'd6: s_cac_x <= writedata[7:0];
+                9'd7: s_cac_y <= writedata[7:0];
+                9'd8: godzilla_x <= writedata[7:0];
+                9'd9: godzilla_y <= writedata[7:0];
+            endcase
+        end else if (VGA_BLANK_n) begin
+            // Background default
+            a <= 8'hFF;
+            b <= 8'hFF;
+            c <= 8'hFF;
+
+            // Dino sprite
+            if (hcount >= dino_x && hcount < dino_x + 32 &&
+                vcount >= dino_y && vcount < dino_y + 32) begin
+                dino_sprite_addr <= (hcount - dino_x) + ((vcount - dino_y) * 32);
+                a <= {dino_sprite_output[15:11], 3'b000};
+                b <= {dino_sprite_output[10:5],  2'b00};
+                c <= {dino_sprite_output[4:0],   3'b000};
+            end
+
+            // Jump sprite
+            if (hcount >= jump_x && hcount < jump_x + 32 &&
+                vcount >= jump_y && vcount < jump_y + 32) begin
+                jump_sprite_addr <= (hcount - jump_x) + ((vcount - jump_y) * 32);
+                a <= {jump_sprite_output[15:11], 3'b000};
+                b <= {jump_sprite_output[10:5],  2'b00};
+                c <= {jump_sprite_output[4:0],   3'b000};
+            end
+
+            // Duck sprite
+            if (hcount >= duck_x && hcount < duck_x + 32 &&
+                vcount >= duck_y && vcount < duck_y + 32) begin
+                duck_sprite_addr <= (hcount - duck_x) + ((vcount - duck_y) * 32);
+                a <= {duck_sprite_output[15:11], 3'b000};
+                b <= {duck_sprite_output[10:5],  2'b00};
+                c <= {duck_sprite_output[4:0],   3'b000};
+            end
+
+            // Small Cactus
+            if (hcount >= s_cac_x && hcount < s_cac_x + 32 &&
+                vcount >= s_cac_y && vcount < s_cac_y + 32) begin
+                scac_sprite_addr <= (hcount - s_cac_x) + ((vcount - s_cac_y) * 32);
+                a <= {scac_sprite_output[15:11], 3'b000};
+                b <= {scac_sprite_output[10:5],  2'b00};
+                c <= {scac_sprite_output[4:0],   3'b000};
+            end
+
+            // Godzilla
+            if (hcount >= godzilla_x && hcount < godzilla_x + 32 &&
+                vcount >= godzilla_y && vcount < godzilla_y + 32) begin
+                godzilla_sprite_addr <= (hcount - godzilla_x) + ((vcount - godzilla_y) * 32);
+                a <= {godzilla_sprite_output[15:11], 3'b000};
+                b <= {godzilla_sprite_output[10:5],  2'b00};
+                c <= {godzilla_sprite_output[4:0],   3'b000};
+            end
+        end
+    end
+
+    assign {VGA_R, VGA_G, VGA_B} = {a, b, c};
+
+endmodule
