de1742838bbaff680f136e9200a0b03d3f5093af
Swapnil Banerjee <60820681+SwapBan@users.noreply.github.com>
2025-04-27 14:14:26 -0400

    Update vga_ball8.sv

--- vga_ball8.sv
+++ vga_ball8.sv
@@ -133,9 +133,9 @@ module vga_ball(
             jump_x <= 8'd200;   jump_y <= 8'd150;
             duck_x <= 8'd300;   duck_y <= 8'd450;
             s_cac_x <= 8'd500;  s_cac_y <= 8'd100;
-            godzilla_x <= 8'd100; godzilla_y <= 8'd360;
+            godzilla_x <= 8'd100; godzilla_y <= 8'd860;
             powerup_x <= 8'd130; powerup_y <= 8'd260;
-            ptr_x <= 8'd700;     ptr_y <= 8'd700;
+            ptr_x <= 8'd700;     ptr_y <= 8'd140;
             // default score
             score   <= 4'd0;
             score_x <= 8'd25;
