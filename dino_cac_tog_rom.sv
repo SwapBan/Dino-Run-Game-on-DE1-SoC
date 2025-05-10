dc4498764bb6b4eb38559d22dfb41bb3475afb2b
roshan2177 <rp3187@columbia.edu>
2025-05-10 15:48:24 -0500

    Update dino_cac_tog_rom.sv

--- dino_cac_tog_rom.sv
+++ dino_cac_tog_rom.sv
@@ -1,16 +1,18 @@
+
 module dino_cacti_together_rom (
     input  logic        clk,
-    input  logic [9:0]  address,
+    input  logic [10:0] address,          // Needs 11 bits for 2048 addresses
     output logic [15:0] data
 );
 
-    logic [15:0] memory [0:1023];
+    logic [15:0] memory [0:2047];         // Increased memory size to 2048
 
     initial begin
-        $readmemh("cactus_group_custom.hex", memory);
+        $readmemh("better_cactus_64x32.hex", memory); // Use new sprite file
     end
 
     always_ff @(posedge clk) begin
         data <= memory[address];
     end
 endmodule
+
