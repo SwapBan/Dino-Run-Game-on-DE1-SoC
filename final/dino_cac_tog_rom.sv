6cfe754ab680a5d76f6dc4275be71745b8e805ae
Swapnil Banerjee <60820681+SwapBan@users.noreply.github.com>
2025-05-14 21:29:00 -0400

    Add files via upload

---
+++ final/dino_cac_tog_rom.sv
@@ -0,0 +1,16 @@
+module dino_cac_tog_rom (
+    input  logic        clk,
+    input  logic [11:0] address,          // More than enough for 2048 entries
+    output logic [15:0] data              // Must be 16 bits for RGB565
+);
+
+    logic [15:0] memory [0:2047];         // Matches your 64×32 sprite
+
+    initial begin
+        $readmemh("better_cactus_64x32.hex", memory);
+    end
+
+    always_ff @(posedge clk) begin
+        data <= memory[address];
+    end
+endmodule
